0583cc6112ec7cff928c7a9cf782052c33f6d821
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-01 21:53:48 +0400

    implemented user route for getting all users for admin only

--- app/user/route.ts
+++ app/user/route.ts
@@ -1,9 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import { validateJWT } from "@/utils/security";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    // Your logic here
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+    const token = req.headers.get("Authorization")?.split(" ")[1];
+
+    if (!token) {
+      return NextResponse.json(
+        { error: "Authorization token is missing" },
+        { status: 401 }
+      );
+    }
+
+    const validToken = await validateJWT(token);
+
+    if (!validToken) {
+      return NextResponse.json(
+        { error: "Invalid or expired token" },
+        { status: 401 }
+      );
+    }
+
+    const tokenUser = await prisma.user.findUnique({
+      where: { id: validToken.id },
+      select: { role: true },
+    });
+
+    if (tokenUser?.role !== "admin") {
+      return NextResponse.json(
+        { error: "You do not have permission to access this resource" },
+        { status: 403 }
+      );
+    }
+
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(

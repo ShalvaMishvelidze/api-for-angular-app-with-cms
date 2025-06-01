a3bf238e0f9ef5ce83704efec659657e98ea5f01
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-01 21:40:24 +0400

    created login route

--- app/user/login/route.ts
+++ app/user/login/route.ts
@@ -1,10 +1,50 @@
+import { prisma } from "@/lib/prisma";
+import { comparePassword, createJWT } from "@/utils/security";
+import { loginSchema } from "@/utils/validators/login";
 import { NextRequest, NextResponse } from "next/server";
+import { ZodError } from "zod";
 
 export async function POST(req: NextRequest) {
   try {
-    // Your logic here
-    return NextResponse.json({ message: "Success" }, { status: 200 });
+    const { email, password } = await req.json();
+
+    const data = loginSchema.parse({ email, password });
+
+    const user = await prisma.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User with this email does not exist" },
+        { status: 401 }
+      );
+    }
+
+    const isValidPassword = await comparePassword(password, user.password);
+
+    if (!isValidPassword) {
+      return NextResponse.json(
+        { error: "Invalid password for this email address" },
+        { status: 401 }
+      );
+    }
+
+    const token = await createJWT({
+      id: user.id,
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+    });
+
+    return NextResponse.json({ token }, { status: 200 });
   } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        { error: error.errors.map((err) => err.message) },
+        { status: 400 }
+      );
+    }
     console.error(error);
     return NextResponse.json(
       { error: "Something went wrong" },

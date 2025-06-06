e23ea10461ad34c593e205dd0bee94a18ee056af
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-06 17:22:24 +0400

    created get product by id route

---
+++ app/product/[id]/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    const product = await prisma.product.findUnique({
+      where: { id },
+    });
+
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}

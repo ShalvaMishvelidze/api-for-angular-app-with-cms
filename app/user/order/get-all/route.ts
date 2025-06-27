d557f5e7027d53eb4ec55e84aa09dde54baefb4a
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-06-27 19:08:41 +0400

    get all orders route

---
+++ app/user/order/get-all/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { validateToken } from "@/utils/tokenValidator";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const validToken = await validateToken(req);
+    const tokenUser = await prisma.user.findUnique({
+      where: { id: validToken.id },
+    });
+    if (!tokenUser) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    const orders = await prisma.order.findMany({
+      where: { userId: tokenUser.id },
+      select: {
+        id: true,
+        status: true,
+        total: true,
+        createdAt: true,
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json({ orders }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
